f48fe1e360d2c51138c72f22735c63dcc505a6db
noebt <noeliabatalle@hotmail.com>
2024-01-26 00:18:42 +0100

    update public path

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/VueInGitHubPages/' //nombre de tu proyecto GitHub
+    : '/'
 });
