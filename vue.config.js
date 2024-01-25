4885b54d7641e988afc0401e928a4ad4a254050a
noebt <noeliabatalle@hotmail.com>
2024-01-26 00:21:20 +0100

    fix

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/VueInGitHubPages/' //nombre de tu proyecto GitHub
+    ? '/lliga-connecting/' //nombre de tu proyecto GitHub
     : '/'
 });
