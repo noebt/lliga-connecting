c08808915e5d1d01accaf732a6e8a11df711588e
noebt <92932606+noebt@users.noreply.github.com>
2024-01-25 23:08:45 +0000

    Deploying to gh-pages from  @ 4b590431d20124719c6dc7a95c1ce0687280bdb9 🚀

---
+++ js/about.c4003ee9.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunklliga_connecting"]=self["webpackChunklliga_connecting"]||[]).push([[443],{4007:function(n,t,a){a.r(t),a.d(t,{default:function(){return f}});var c=a(3396);const e={class:"about"},u=(0,c._)("h1",null,"This is an about page",-1),i=[u];function r(n,t){return(0,c.wg)(),(0,c.iD)("div",e,i)}var s=a(89);const l={},o=(0,s.Z)(l,[["render",r]]);var f=o}}]);
+//# sourceMappingURL=about.c4003ee9.js.map
